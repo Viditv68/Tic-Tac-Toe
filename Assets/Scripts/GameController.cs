6aad6e3cc9b2d2abf938fa9d51e8f24129de6c0f
Viditv68 <viditrawatv68@gmail.com>
2021-01-28 10:14:01 +0530

    GameController

---
+++ Assets/Scripts/GameController.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GameController : MonoBehaviour
+{
+    public int whoseTurn;
+    public int turnCount;
+    public Sprite[] playIcons;
+    public Button[] ticTacToeSpaces;
+
+    private int[] whichPlayerMarked;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        GameSetup();
+    }
+
+    private void GameSetup()
+    {
+        whoseTurn = 0;
+        turnCount = 0;
+
+        for(int i = 0; i < ticTacToeSpaces.Length; i++)
+        {
+            ticTacToeSpaces[i].interactable = true;
+            ticTacToeSpaces[i].GetComponent<Image>().sprite = null;
+            whichPlayerMarked[i] = -1;
+        }
+
+
+    }
+
+
+    public void TicTacToeButton(int index)
+    {
+        ticTacToeSpaces[index].image.sprite = playIcons[whoseTurn];
+        ticTacToeSpaces[index].interactable = false;
+
+        whichPlayerMarked[index] = whoseTurn;
+        turnCount++;
+
+        if(whoseTurn == 0)
+        {
+            whoseTurn = 1;
+        }
+        else
+        {
+            whoseTurn = 0;
+        }
+    }
+}
