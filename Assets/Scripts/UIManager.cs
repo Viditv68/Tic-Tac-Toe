555175ca0ddd37c6ea347336ecf0a6da77f0d87a
Viditv68 <viditrawatv68@gmail.com>
2021-01-29 10:07:06 +0530

    Added MainMenu and gameplay screen to dictionary

---
+++ Assets/Scripts/UIManager.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+
+
+[Serializable]
+public class UIScreen
+{
+    public GameObject MainMenu;
+    public GameObject GameplayScreen;
+}
+
+
+public class UIManager : MonoBehaviour
+{
+    [SerializeField]
+    private Button playButton;
+
+    public Dictionary<string, GameObject> Screen;
+
+    public UIScreen uiScreen;
+    private void Awake()
+    { 
+        playButton.onClick.AddListener(StartGame);
+        
+    }
+    private void Start()
+    {
+        Screen = new Dictionary<string, GameObject>();
+        
+        Screen.Add("MainMenu", uiScreen.MainMenu);
+        Screen.Add("Gameplay", uiScreen.GameplayScreen);
+    }
+
+    private void StartGame()
+    {
+        GameObject obj = null;
+
+        if(Screen.TryGetValue("MainMenu", out obj))
+        {
+            obj.SetActive(false);
+        }
+        if(Screen.TryGetValue("Gameplay", out obj))
+        {
+            obj.SetActive(true);
+        }
+    }
+}
