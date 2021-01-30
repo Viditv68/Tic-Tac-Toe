d870cbc3116ccc09b61b9773be11f7d9cbb04c4c
Viditv68 <viditrawatv68@gmail.com>
2021-01-30 17:01:22 +0530

    editor script

---
+++ Assets/Editor/AssetEditor.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using UnityEditor;
+using System.Collections.Generic;
+
+[CustomEditor(typeof(AssetsScript))]
+public class AssetEditor : Editor
+{
+
+    public override void OnInspectorGUI()
+    {
+        base.OnInspectorGUI();
+
+        List<GameObject> gameobjs = new List<GameObject>();
+
+        foreach (GameObject obj in Object.FindObjectsOfType(typeof(GameObject)))
+        {
+            gameobjs.Add(obj);
+        }
+
+        foreach(GameObject obj in gameobjs)
+        {
+            if (GUILayout.Button(obj.name))
+            {
+                AssetsScript asset = (AssetsScript)target;
+                asset.GetAssets();
+            }
+        }
+        
+    }
+}
---
+++ Assets/Scripts/AssetsScript.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+
+public class AssetsScript : MonoBehaviour
+{
+    private GameObject[] gameObjects;
+
+    void Start()
+    {
+    }
+
+    public void GetAssets()
+    {
+        Debug.Log(Application.dataPath);
+        string[] str = AssetDatabase.GetAllAssetPaths();
+
+        foreach(string s in str)
+        {
+            if(s.Contains("/Assets"))
+            {
+                Debug.Log(AssetDatabase.AssetPathToGUID(s));
+
+                Object[] obj = AssetDatabase.LoadAllAssetsAtPath(s);
+
+                EditorUtility.CollectDependencies(AssetDatabase.LoadAllAssetsAtPath(s));
+
+            }
+        }
+
+    }
+}
